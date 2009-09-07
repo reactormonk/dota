b467b640e53d3f04e9a95fd609bbf496c9cd0a4a
Simon Hafner <hafnersimon@gmail.com>
2009-09-07 21:36:31 +0200

    added specs

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,6 +2,64 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe Game do
 
-  it "should have specs"
+  it 'should stage' do
+
+    it 'should be created associated to a league'
+
+    it 'should be possible for players to join'
+
+    it 'should not allow a player to join if...' do
+      
+      it "... he's banned"
+
+      it "... he's not vouched"
+
+      it "... he's playing already"
+
+    end
+
+    it 'should be possible to set a mode'
+
+    it 'should implement various player distribution mechanism' do
+
+      it 'should implement captains'
+
+      it 'should implement (intelligent) random assignment'
+
+    end
+
+    it "should grab the player's score from his LeagueMembership"
+
+    it 'should be destroyed if the last player left the game'
+
+    it 'should be destroyable even with players staged/assigned'
+
+  end
+
+  it 'should start' do
+
+    it 'should start only with 5 players each'
+
+    it 'should set start_time'
+
+    it 'should not allow joining/leaving players'
+
+    it 'should drop staged (not-assigned) players'
+
+  end
+
+  it 'should finish' do
+
+    it 'should accept votes' do
+
+      it 'should be possible to abort the game'
+
+      it 'should be possible for sentinel/scourge to win the game'
+
+    end
+    
+    it 'should only finish with a replay [magic file check?]'
+
+  end
 
 end
--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -2,6 +2,8 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe League do
 
-  it "should have specs"
+  it 'should be possible for players to be vouched'
+
+  it 'should be possible to ban players'
 
 end
