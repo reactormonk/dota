a0582673b395950f3652d2025f0c6b78fe5f8807
Simon Hafner <hafnersimon@gmail.com>
2009-10-29 14:23:29 +0100

    moved some specs from game to game_membership

--- spec/models/game_membership_spec.rb
+++ spec/models/game_membership_spec.rb
@@ -2,4 +2,20 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe GameMembership do
 
+  it "should grab the player's score from his LeagueMembership" do
+    p,g,l = pgl_vouch
+    p.league_memberships.first(:league => l).score = 1200.0
+    p.league_memberships.all.each {|mem| mem.save} # Do not ask....
+    p.join(g)
+    g.game_memberships.first(:player => p).score.should == p.league_memberships.first(:league => l).score
+  end
+
+  def pgl_vouch
+    l = League.gen
+    p = Player.gen
+    g = Game.gen
+    p.vouch(l)
+    [p,g,l]
+  end
+
 end
