7cc674720a8e0bc821e4974653d9ff387b24b41c
Simon Hafner <hafnersimon@gmail.com>
2009-12-06 22:13:54 +0100

    implemented game/join

--- app/controllers/games.rb
+++ app/controllers/games.rb
@@ -1,4 +1,5 @@
 class Games < Application
+  before :ensure_authenticated, :exclude => [:show, :index]
 
   def index
     @games = Game.all
@@ -8,6 +9,20 @@ class Games < Application
   def show
     @game = Game.first(:id => params[:id])
     display @game
+  end
+
+  def join
+    @game = Game.first(:id => params[:id])
+    begin
+      @game.join(session.user)
+    rescue PlayerPlaying
+      message[:error] = "Du spielt schon in Game ##{session.user.where_playing.id}"
+    rescue NotVouched
+      message[:error] = "Lass ich erstmal in #{@game.league} vouchen"
+    else
+      message[:notice] = "Alles paletti, du bist in Game ##{@game.id}."
+    end
+    redirect resource(@game), :message => message
   end
   
 end
--- config/router.rb
+++ config/router.rb
@@ -29,7 +29,9 @@ Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do
   resources :players, :identify => :login
   resources :leagues, :identify => :name
-  resources :games
+  resources :games do
+    member :join    
+  end
   
   # Adds the required routes for merb-auth using the password slice
   slice(:merb_auth_slice_password, :name_prefix => nil, :path_prefix => "")
