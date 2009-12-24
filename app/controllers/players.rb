f114fa22dafb0b3bc1c7cd0241d0f119b3bf5f8d
Simon Hafner <hafnersimon@gmail.com>
2009-12-24 14:35:39 +0100

    added leagues and players templates

--- app/controllers/players.rb
+++ app/controllers/players.rb
@@ -3,13 +3,15 @@ class Players < Application
 
   def index
     @players = Player.all
-    display @players
+    render "players/index"
   end
   
   def show
     @player = Player.first(:login => Merb::Parse.unescape(params[:login]))
+    puts @player
     raise NotFound unless @player
-    display @player
+    @game = @player.where_playing
+    render "players/show"
   end
 
 end
