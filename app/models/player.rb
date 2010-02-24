ed48c6823e70790bb030d569502ec6538ded018b
Simon Hafner <hafnersimon@gmail.com>
2010-02-24 23:01:27 +0100

    player cleanup

--- app/models/player.rb
+++ app/models/player.rb
@@ -28,13 +28,6 @@ class Player
   #
   # Logic
   #
-  def join(game)
-    game.join(self)
-  end
-
-  def join_as_captain(game)
-    game.join_as_captain(self)
-  end
 
   def leave
     if (game = where_playing)
@@ -56,28 +49,12 @@ class Player
     game_memberships.first(:game => where_playing).party
   end
 
-  def check_playing
-    where_playing or PlayerPlaying.new(self)
-  end
-
   def accept_challenge
-    captain_game = challenged 
+    captain_game = challenged
     unless captain_game && game_memberships.first(:game => captain_game).party == :scourge
       raise NotChallenged, "You're not challenged."
     end
     # TODO
-  end
-
-  # I'm not even sure if you should use this method... it's just for spec backwards compability ;-)
-  def challenge(league, player=nil)
-    if captain_game = challenged
-      raise PlayerPlaying, "You're playing in #{captain_game}."
-    elsif player
-      captain_game = league.direct_challenge(self, player)
-    else
-      captain_game = league.anonymous_challenge(self)
-    end
-    captain_game
   end
 
   def challenged
@@ -86,14 +63,6 @@ class Player
 
   def challenged?
     !! challenged
-  end
-  
-  def vote(chosen)
-    where_playing.vote(self, chosen)
-  end
-
-  def score(league)
-    league_memberships.first(:league => league).score
   end
 
 end
