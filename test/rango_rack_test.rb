e07554174d1de33959fc5be9788c2084fbe3dfbf
Simon Hafner <hafnersimon@gmail.com>
2010-03-18 21:31:06 +0100

    fixed tests

--- test/rango_rack_test.rb
+++ test/rango_rack_test.rb
@@ -1,9 +1,10 @@
 BareTest.new_component :rango_rack_test do
   require 'rack/test'
+  require 'rango/utils'
 
   module Rack::Test::Methods
     def app
-      Rango::Router.app
+      Rango::Utils.load_rackup
     end
   end
   BareTest::Assertion::Context.send :include, Rack::Test::Methods
